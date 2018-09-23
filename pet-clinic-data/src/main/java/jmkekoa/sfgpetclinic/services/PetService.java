3ec6a66ae07617bab65f6acf661cfefaad77b314
jkekoamartin <james.kekoa.martin@gmail.com>
2018-09-23 17:50:24 -0500

    Refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/jmkekoa/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/jmkekoa/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package jmkekoa.sfgpetclinic.services;
 
 import jmkekoa.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
