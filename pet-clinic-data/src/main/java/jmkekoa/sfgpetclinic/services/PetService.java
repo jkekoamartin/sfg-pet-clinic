f7d91f261db962db38e0a38da69e4b37e110f968
jkekoamartin <james.kekoa.martin@gmail.com>
2018-09-23 16:52:28 -0500

    Implement interfaces for service. Closes #5

---
+++ pet-clinic-data/src/main/java/jmkekoa/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package jmkekoa.sfgpetclinic.services;
+
+import jmkekoa.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
